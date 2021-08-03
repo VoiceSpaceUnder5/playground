7d98831106193a564d01021d1731b788a8c53a84
Mj J <minje70@naver.com>
2021-08-03 17:35:31 +0900

    네비게이션...
    제대로 올렸습니다...

---
+++ test-nav/src/NavigationTab.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function NavigationTab() {
+	return (
+		<div className='Navigation'>
+			<button>kilee</button>
+			<button>마이크</button>
+			<button>화면</button>
+			<button>...</button>
+			<button>나가기</button>
+			<button>올라오는거</button>
+		</div>
+	)
+}
+
+export default NavigationTab
