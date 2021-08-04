0021e8ff2b0afc306bf8a4d313d2b2617d0f07c4
Likilee <gnsdlrl@daum.net>
2021-08-04 09:51:45 +0900

    WebGL 튜토리얼 기초 학습 파일 생성
    WebGL 초기화까지 코드작업
    이만 TS, REACT를 학습으로 넘어가자
    혼리님에게 WebGL 맡기고  ㅌㅌ

---
+++ WebGL/WebGL기초/app.js
@@ -0,0 +1,63 @@
+let canvas = document.querySelector("#canvas");
+
+let gl = canvas.getContext("webgl");
+console.dir(gl);
+
+if (!gl) {
+  console.log("WebGL이 없어요!");
+}
+
+function createShader(gl, type, source) {
+  var shader = gl.createShader(type);
+  gl.shaderSource(shader, source);
+  gl.compileShader(shader);
+
+  var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+  if (success) {
+    return shader;
+  }
+
+  console.log(gl.getShaderInfoLog(shader));
+  gl.deleteShader(shader);
+}
+
+const vertexShaderSource = document.querySelector("#vertex-shader-2d").text;
+const fragmentShaderSource = document.querySelector("#fragment-shader-2d").text;
+
+const vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
+const fragmentShader = createShader(
+  gl,
+  gl.FRAGMENT_SHADER,
+  fragmentShaderSource
+);
+
+function createProgram(gl, vertexShader, fragmentShader) {
+  var program = gl.createProgram();
+  gl.attachShader(program, vertexShader);
+  gl.attachShader(program, fragmentShader);
+  gl.linkProgram(program);
+
+  var success = gl.getProgramParameter(program, gl.LINK_STATUS);
+  if (success) {
+    return program;
+  }
+
+  console.log(gl.getProgramInfoLog(program));
+  gl.deleteProgram(program);
+}
+
+const program = createProgram(gl, vertexShader, fragmentShader);
+
+const positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+const positionBuffer = gl.createBuffer();
+
+gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+const positions = [0, 0, 0, 0.5, 0.7, 0];
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+
+//초기화 끝
+
+//랜더링
+
+gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
